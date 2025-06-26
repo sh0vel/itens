456668ecd6158c56814487f0c5e4ce3ada362d71
Shovon Hossain <shovonsemail@gmail.com>
2025-06-25 22:05:20 -0400

    iens

---
+++ london-itinerary/main.js
@@ -0,0 +1,13 @@
+// Loads each day's table into the correct div
+function loadTable(id, file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+        });
+}
+
+loadTable('day1-table', 'day1.html');
+loadTable('day2-table', 'day2.html');
+loadTable('day3-table', 'day3.html');
+loadTable('day4-table', 'day4.html');
